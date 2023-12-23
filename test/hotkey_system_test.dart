fb2198661acd7261023e1e08e5b0b0d051720f4b
SUY <suy.register@gmail.com>
2023-12-23 09:50:23 +0300

    Fix import order in hotkey_system_test.dart

--- test/hotkey_system_test.dart
+++ test/hotkey_system_test.dart
@@ -1,6 +1,9 @@
 // import 'package:flutter/services.dart';
-import 'package:flutter_test/flutter_test.dart';
+
 // import 'package:hotkey_system/hotkey_system.dart';
+
+
+import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   // const MethodChannel channel = MethodChannel('hotkey_system');
