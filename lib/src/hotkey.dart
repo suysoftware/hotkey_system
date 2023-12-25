ba956c536733a1704e93e6b89176eb2a963a5e02
SUY <suy.register@gmail.com>
2023-12-25 16:48:54 +0300

    Refactor HotKey.fromJson method to use KeyModifierParser for modifiers

--- lib/src/hotkey.dart
+++ lib/src/hotkey.dart
@@ -1,6 +1,5 @@
 import 'package:hotkey_system/src/enums/key_code.dart';
 import 'package:hotkey_system/src/enums/key_modifier.dart';
-import 'package:uuid/uuid.dart';
 
 enum HotKeyScope {
   system,
@@ -21,7 +20,9 @@ class HotKey {
   factory HotKey.fromJson(Map<String, dynamic> json) {
     return HotKey(
       KeyCodeParser.parse(json['keyCode']),
-      modifiers: List<String>.from(json['modifiers']).map((e) => KeyModifierParser.parse(e)).toList(),
+      modifiers: List<String>.from(json['modifiers'])
+          .map((e) => KeyModifierParser.parse(e))
+          .toList(),
       identifier: json['identifier'],
       scope: HotKeyScope.values.firstWhere(
         (e) => e.name == json['scope'],
@@ -32,7 +33,7 @@ class HotKey {
 
   KeyCode keyCode;
   List<KeyModifier>? modifiers;
-  String identifier = const Uuid().v4();
+  String identifier = DateTime.now().millisecondsSinceEpoch.toString();
   HotKeyScope scope = HotKeyScope.system;
 
   Map<String, dynamic> toJson() {
